8a61041e7e4370cf2de1bb5ead4a5961360e0c26
Permitted <permittedbr@gmail.com>
2022-07-18 23:30:17 +0300

    auth & model

--- authentication-service/cmd/api/routes.go
+++ authentication-service/cmd/api/routes.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"github.com/go-chi/chi/v5"
-	"github.com/go-chi/chi/v5/middleware"
 	"github.com/go-chi/cors"
 	"net/http"
 )
@@ -19,10 +18,6 @@ func (app *Config) routes() http.Handler {
 		AllowCredentials: true,
 		MaxAge:           300,
 	}))
-
-	mux.Use(middleware.Heartbeat("/ping"))
-
-	mux.Post("/", app.Broker)
 
 	return mux
 }
